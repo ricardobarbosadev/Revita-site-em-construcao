9b42810d623424570941f2839b6adc052d7dcb12
Ricardo <ricardo-jsb@hotmail.com>
2025-02-12 18:31:01 +0000

    Versão inicial do site em construção

---
+++ src/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Página carregada. Site em construção!");
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
