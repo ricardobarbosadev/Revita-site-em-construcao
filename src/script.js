c2144e1eceec05833ca2447db1298dbdc9abcf8d
Ricardo <ricardo-jsb@hotmail.com>
2025-05-02 15:27:33 +0100

    Merge branch development into correção-whatsApp-link

--- src/script.js
+++ src/script.js
@@ -1,11 +1,33 @@
+// Meta de rastreamento
 window.va =
   window.va ||
   function () {
     (window.vaq = window.vaq || []).push(arguments);
   };
 
+// Espera o DOM estar completamente carregado
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Página carregada. Site em construção!");
-});
 
-document.getElementById("year").textContent = new Date().getFullYear();
+  // Atualiza o ano automaticamente
+  const yearElement = document.getElementById("year");
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+
+  // número de WhatsApp vindo do .env
+  const phone = import.meta.env.VITE_WHATSAPP_PHONE;
+  const whatsappLink = `https://wa.me/${phone}`;
+
+  // Atualiza botão "Peça um Orçamento"
+  const cta = document.querySelector(".cta");
+  if (cta) {
+    cta.setAttribute("href", whatsappLink);
+  }
+
+  // Atualiza ícone do header
+  const whatsappHeader = document.getElementById("whatsapp-header");
+  if (whatsappHeader) {
+    whatsappHeader.setAttribute("href", whatsappLink);
+  }
+});
