e6278b319c0a4ad5a8e4f89cd371b93584e140b5
Ricardo <ricardo-jsb@hotmail.com>
2025-05-02 15:27:33 +0100

    Merge branch development into correção-whatsApp-link

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // raiz continua a ser a pasta atual
+  build: {
+    outDir: "dist", // pasta de saída padrão
+  },
+});
